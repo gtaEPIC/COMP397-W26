c075056ef77c4cec0e99bce5b8be605007d96a59
Johnathan-Hall <johnnygotrain@gmail.com>
2026-02-09 21:01:15 -0500

    Lab 3 Part 1
    The lab 2 fix

--- Labs/Assets/Scripts/PlayerInput.cs
+++ Labs/Assets/Scripts/PlayerInput.cs
@@ -13,6 +13,7 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float gravity = -9.87f;
     [SerializeField] private float rotationSpeed = 4.0f;
     [SerializeField] private float mouseSensY = 4.0f;
+    private float camXRotation;
     private Vector3 velocity;
     [SerializeField, Self] private CharacterController controller;
     [SerializeField, Child] private Camera cam;
@@ -22,6 +23,8 @@ public class PlayerInput : MonoBehaviour
     {
         move = InputSystem.actions.FindAction("Player/Move");
         look = InputSystem.actions.FindAction("Player/Look");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -41,9 +44,9 @@ public class PlayerInput : MonoBehaviour
         transform.Rotate(Vector3.up, readLook.x * rotationSpeed * Time.deltaTime);
         
         // Rotate the camera
-        mouseSensY *= readLook.y;
-        mouseSensY = Mathf.Clamp(mouseSensY, -90f, 90f);
-        cam.gameObject.transform.localRotation = Quaternion.Euler(mouseSensY * readLook.y, 0, 0);
+        camXRotation += mouseSensY * readLook.y * Time.deltaTime * -1;
+        camXRotation = Mathf.Clamp(camXRotation, -90f, 90f);
+        cam.gameObject.transform.localRotation = Quaternion.Euler(camXRotation, 0, 0);
     }
 
     private void OnValidate()
